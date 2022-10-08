60aa7795c566a22377afa1d1d40f16cfb93992cc
AFGHAN HACK <110977192+AfghanHack001@users.noreply.github.com>
2022-10-08 15:59:24 +0430

    Create fast.py

---
+++ fast.py
@@ -0,0 +1,14 @@
+import os,time
+os.system('git pull')
+os.system('clear')
+
+print('\033[91;1m  Facebook Is On Update ')
+print('')
+print('')
+print('\033[92;1m  So Chance Of Just Now Login Is Only 30% ')
+print()
+print()
+print()
+print('\033[95;1m  Wait And Read & You Will Be Directly Go On Cloning Menu In 5 Seconds');time.sleep(5)
+import fast
+fast.Main()
